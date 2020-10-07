e446881c3dd8a4b3b9b57d3301265348d8d33f89
yugimamidi <yugi.mamidi01@gmail.com>
2020-10-06 20:29:37 -0500

    updated with department mapper

---
+++ shopping_mapper.py
@@ -0,0 +1,20 @@
+# Case 1 - Mapper using Files
+# Easy to test
+# Not quite Hadoop-ready
+
+# open returns a file object
+with open("part.txt", "r") as input:
+  with open("department_mapped.txt", "w") as output:
+
+    # iterate through each line in the file object
+    for line in input:
+      datalist = line.strip().split("    ")
+      if (len(datalist) == 6) : 
+        date, time,store, department, amount, paymentType = datalist
+
+        # output intermediate key-value pairs
+        output.write(department + "\t" + amount + "\n")
+
+        # display to console (not required - just for the user)
+        print(department + "\t" + amount + "\n")
+
